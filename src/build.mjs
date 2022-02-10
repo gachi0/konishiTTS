32610cca726c04244d63f0fd9074c2e62eaced91
gachi0 <51742340+gachi0@users.noreply.github.com>
2022-02-10 22:52:36 +0900

    ビルド用の設定を追加

---
+++ src/build.mjs
@@ -0,0 +1,20 @@
+import pkg from "pkg";
+import { exec } from "child_process";
+import { promisify } from "util";
+import fs from "fs";
+import archiver from "archiver";
+
+await promisify(exec)("npx tsc");
+await pkg.exec([".", "--out-path", "./dist/exes"]);
+
+for (const e of fs.readdirSync("dist/exes")) {
+    const out = fs.createWriteStream(`dist/${e.split(".")[0]}.zip`);
+    const archive = archiver("zip", { zlib: { level: 9 } })
+        .file("config.toml.sample", { name: "config.toml" })
+        .file(`dist/exes/${e}`, { name: e });
+    archive.pipe(out);
+    await archive.finalize();
+    fs.rmSync(`dist/exes/${e}`);
+}
+
+fs.rmdirSync("dist/exes");
